04ef6001239ca58c96e5726986b83f3363963811
thehashmap <nkmnikhil0009@gmail.com>
2023-07-30 14:21:04 +0530

    restructure code

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// src/index.ts
-const fs_1 = __importDefault(require("fs"));
 const axios_1 = __importDefault(require("axios"));
 const client_1 = require("@prisma/client");
 const dotenv_1 = __importDefault(require("dotenv"));
@@ -12,11 +10,24 @@ dotenv_1.default.config(); // Load environment variables from .env file
 const prisma = new client_1.PrismaClient();
 // Utility function to fetch transaction info from Etherscan API
 async function fetchTransactionInfo(walletAddress) {
-    const etherscanBaseUrl = "https://api.etherscan.io/api";
+    const etherscanBaseUrl = "https://api-goerli.etherscan.io/api";
     const apiKey = process.env.ETHERSCAN_API_KEY; // Access API key from environment variable
-    const url = `${etherscanBaseUrl}?module=account&action=txlist&address=${walletAddress}&apiKey=${apiKey}`;
+    const uniTokenAddress = "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984";
+    const sushiTokenAddress = "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2";
     try {
-        const response = await axios_1.default.get(url);
+        // Get the current timestamp (Unix timestamp) in seconds
+        const currentTimestampInSeconds = Math.floor(Date.now() / 1000);
+        // Get the latest block number from the Etherscan API
+        const latestBlockNumberResponse = await axios_1.default.get(`${etherscanBaseUrl}?module=block&action=getblocknobytime&timestamp=${currentTimestampInSeconds}&closest=before&apiKey=${apiKey}`);
+        const latestBlockNumber = latestBlockNumberResponse.data.result;
+        console.log("latestBlockNumber: ", latestBlockNumber);
+        // Use the latest block number to update the erc20txns URL
+        // const erc20txns = `${etherscanBaseUrl}?module=account&action=tokentx&contractaddress=${uniTokenAddress}&address=${walletAddress}&startblock=0&endblock=${latestBlockNumber}&page=1&offset=1000&apiKey=${apiKey}`;
+        // const erc20txns = `${etherscanBaseUrl}?module=account&action=tokentx&address=${walletAddress}&startblock=0&endblock=${latestBlockNumber}&page=1&offset=1000&sort=asc&apiKey=${apiKey}`;
+        const txnsList = `${etherscanBaseUrl}?module=account&action=txlist&address=${walletAddress}&startblock=0&endblock=${latestBlockNumber}&page=1&offset=1000&sort=asc&apiKey=${apiKey}`;
+        // Fetch ERC20 transactions using the updated URL
+        const response = await axios_1.default.get(txnsList);
+        console.log(response.data);
         return response.data.result;
     }
     catch (error) {
@@ -29,13 +40,8 @@ async function fetchTransactionInfo(walletAddress) {
 async function fetchAndSaveData() {
     try {
         // Load wallet addresses from a JSON file (you can add more addresses here)
-        const walletAddresses = JSON.parse(fs_1.default.readFileSync("./src/wallet-addresses.json", "utf8"));
-        // Get the processed addresses from the JSON file
-        const processedAddresses = JSON.parse(fs_1.default.readFileSync("./src/processed-addresses.json", "utf8"));
-        const processedAddressSet = new Set(processedAddresses);
-        // Filter out the addresses that haven't been processed yet
-        const remainingAddresses = walletAddresses.filter((address) => !processedAddressSet.has(address));
-        for (const walletAddress of remainingAddresses) {
+        const walletAddresses = ["0x34d014758297c00FeA49935FCe172677904d51EF"]; // Update with your desired wallet addresses
+        for (const walletAddress of walletAddresses) {
             const transactionInfo = await fetchTransactionInfo(walletAddress);
             if (transactionInfo.length > 0) {
                 await prisma.wallet.create({
@@ -45,19 +51,30 @@ async function fetchAndSaveData() {
                             createMany: {
                                 data: transactionInfo.map((transaction) => ({
                                     hash: transaction.hash,
-                                    blockNumber: parseInt(transaction.blockNumber),
+                                    blockNumber: transaction.blockNumber,
+                                    timeStamp: new Date(parseInt(transaction.timeStamp) * 1000),
                                     from: transaction.from,
                                     to: transaction.to,
                                     value: transaction.value,
-                                    timestamp: new Date(parseInt(transaction.timeStamp) * 1000),
+                                    nonce: transaction.nonce,
+                                    blockHash: transaction.blockHash,
+                                    transactionIndex: transaction.transactionIndex,
+                                    gas: transaction.gas,
+                                    gasPrice: transaction.gasPrice,
+                                    isError: transaction.isError,
+                                    txreceipt_status: transaction.txreceipt_status,
+                                    input: transaction.input,
+                                    contractAddress: transaction.contractAddress,
+                                    cumulativeGasUsed: transaction.cumulativeGasUsed,
+                                    gasUsed: transaction.gasUsed,
+                                    confirmations: transaction.confirmations,
+                                    methodId: transaction.methodId,
+                                    functionName: transaction.functionName,
                                 })),
                             },
                         },
                     },
                 });
-                // Add the processed address to the JSON file to avoid duplicates
-                processedAddresses.push(walletAddress);
-                fs_1.default.writeFileSync("./src/processed-addresses.json", JSON.stringify(processedAddresses, null, 2));
             }
         }
         console.log("Data fetching and saving complete.");
--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,31 @@ const prisma = new PrismaClient();
 
 // Utility function to fetch transaction info from Etherscan API
 async function fetchTransactionInfo(walletAddress: string): Promise<any> {
-  const etherscanBaseUrl = "https://api.etherscan.io/api";
+  const etherscanBaseUrl = "https://api-goerli.etherscan.io/api";
   const apiKey = process.env.ETHERSCAN_API_KEY; // Access API key from environment variable
-  const url = `${etherscanBaseUrl}?module=account&action=txlist&address=${walletAddress}&apiKey=${apiKey}`;
+
+  const uniTokenAddress = "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984";
+  const sushiTokenAddress = "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2";
 
   try {
-    const response = await axios.get(url);
+    // Get the current timestamp (Unix timestamp) in seconds
+    const currentTimestampInSeconds = Math.floor(Date.now() / 1000);
+
+    // Get the latest block number from the Etherscan API
+    const latestBlockNumberResponse = await axios.get(
+      `${etherscanBaseUrl}?module=block&action=getblocknobytime&timestamp=${currentTimestampInSeconds}&closest=before&apiKey=${apiKey}`
+    );
+    const latestBlockNumber = latestBlockNumberResponse.data.result;
+    console.log("latestBlockNumber: ", latestBlockNumber);
+
+    // Use the latest block number to update the erc20txns URL
+    // const erc20txns = `${etherscanBaseUrl}?module=account&action=tokentx&contractaddress=${uniTokenAddress}&address=${walletAddress}&startblock=0&endblock=${latestBlockNumber}&page=1&offset=1000&apiKey=${apiKey}`;
+    // const erc20txns = `${etherscanBaseUrl}?module=account&action=tokentx&address=${walletAddress}&startblock=0&endblock=${latestBlockNumber}&page=1&offset=1000&sort=asc&apiKey=${apiKey}`;
+    const txnsList = `${etherscanBaseUrl}?module=account&action=txlist&address=${walletAddress}&startblock=0&endblock=${latestBlockNumber}&page=1&offset=1000&sort=asc&apiKey=${apiKey}`;
+
+    // Fetch ERC20 transactions using the updated URL
+    const response = await axios.get(txnsList);
+    console.log(response.data);
     return response.data.result;
   } catch (error) {
     // Explicitly cast 'error' to 'Error' type to resolve TypeScript error
@@ -32,22 +51,9 @@ async function fetchTransactionInfo(walletAddress: string): Promise<any> {
 async function fetchAndSaveData(): Promise<void> {
   try {
     // Load wallet addresses from a JSON file (you can add more addresses here)
-    const walletAddresses = JSON.parse(
-      fs.readFileSync("./src/wallet-addresses.json", "utf8")
-    );
+    const walletAddresses = ["0x34d014758297c00FeA49935FCe172677904d51EF"]; // Update with your desired wallet addresses
 
-    // Get the processed addresses from the JSON file
-    const processedAddresses = JSON.parse(
-      fs.readFileSync("./src/processed-addresses.json", "utf8")
-    );
-    const processedAddressSet = new Set(processedAddresses);
-
-    // Filter out the addresses that haven't been processed yet
-    const remainingAddresses = walletAddresses.filter(
-      (address: string) => !processedAddressSet.has(address)
-    );
-
-    for (const walletAddress of remainingAddresses) {
+    for (const walletAddress of walletAddresses) {
       const transactionInfo = await fetchTransactionInfo(walletAddress);
       if (transactionInfo.length > 0) {
         await prisma.wallet.create({
@@ -57,23 +63,30 @@ async function fetchAndSaveData(): Promise<void> {
               createMany: {
                 data: transactionInfo.map((transaction: any) => ({
                   hash: transaction.hash,
-                  blockNumber: parseInt(transaction.blockNumber),
+                  blockNumber: transaction.blockNumber,
+                  timeStamp: new Date(parseInt(transaction.timeStamp) * 1000),
                   from: transaction.from,
                   to: transaction.to,
                   value: transaction.value,
-                  timestamp: new Date(parseInt(transaction.timeStamp) * 1000),
+                  nonce: transaction.nonce,
+                  blockHash: transaction.blockHash,
+                  transactionIndex: transaction.transactionIndex,
+                  gas: transaction.gas,
+                  gasPrice: transaction.gasPrice,
+                  isError: transaction.isError,
+                  txreceipt_status: transaction.txreceipt_status,
+                  input: transaction.input,
+                  contractAddress: transaction.contractAddress,
+                  cumulativeGasUsed: transaction.cumulativeGasUsed,
+                  gasUsed: transaction.gasUsed,
+                  confirmations: transaction.confirmations,
+                  methodId: transaction.methodId,
+                  functionName: transaction.functionName,
                 })),
               },
             },
           },
         });
-
-        // Add the processed address to the JSON file to avoid duplicates
-        processedAddresses.push(walletAddress);
-        fs.writeFileSync(
-          "./src/processed-addresses.json",
-          JSON.stringify(processedAddresses, null, 2)
-        );
       }
     }
 
