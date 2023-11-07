d25b29fd5ee1e6b8bb8f53138c9fa7077f2298b8
thehashmap <nkmnikhil0009@gmail.com>
2023-11-07 23:53:02 +0530

    store in db

--- src/getWalletBalanceHistory.ts
+++ src/getWalletBalanceHistory.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Wallet, WalletStatus } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -16,7 +16,37 @@ function convertBalanceToEther(balance: bigint): string {
   return etherBalance;
 }
 
-async function getWalletBalanceHistory(walletAddress: string): Promise<BalanceChange[]> {
+// Function to delete existing data for a wallet with the INDEXING status
+async function deleteDataForIndexingWallet(wallet: Wallet): Promise<void> {
+  try {
+    // Delete history associated with the wallet
+    await prisma.balanceHistory.deleteMany({
+      where: {
+        walletId: wallet.id,
+      },
+    });
+
+    console.log(`Deleted data for wallet ${wallet.address} with INDEXING status.`);
+  } catch (error) {
+    console.error(`Error deleting data for wallet ${wallet.address}:`, error);
+  }
+}
+
+async function saveBalanceHistoryToDB(walletId: string, balanceChange: BalanceChange): Promise<void> {
+  await prisma.balanceHistory.create({
+    data: {
+      wallet: {
+        connect: { id: walletId },
+      },
+      timeStamp: balanceChange.timeStamp,
+      currentBalance: convertBalanceToEther(balanceChange.currentBalance),
+      previousBalance: convertBalanceToEther(balanceChange.previousBalance),
+      balanceChange: convertBalanceToEther(balanceChange.balanceChange),
+    },
+  });
+}
+
+async function getWalletBalanceHistory(walletId: string, walletAddress: string): Promise<BalanceChange[]> {
   const balanceChanges: BalanceChange[] = [];
   let currentBalance = BigInt(0);
   let previousBalance = BigInt(0);
@@ -60,6 +90,7 @@ async function getWalletBalanceHistory(walletAddress: string): Promise<BalanceCh
         const gasUsed = BigInt(tx.gasUsed);
         const gasPrice = BigInt(tx.gasPrice);
         gasFees = gasUsed * gasPrice;
+        balanceChange -= gasFees;
       }
     }
 
@@ -75,7 +106,7 @@ async function getWalletBalanceHistory(walletAddress: string): Promise<BalanceCh
 
     // Apply the balance change, record gas fees, and record if the transaction was reverted
     previousBalance = currentBalance;
-    currentBalance += balanceChange - gasFees;
+    currentBalance += balanceChange;
     balanceChanges.push({
       timeStamp: tx.timeStamp,
       currentBalance,
@@ -87,10 +118,19 @@ async function getWalletBalanceHistory(walletAddress: string): Promise<BalanceCh
     // Print balance change for each transaction
     const currentBalanceEther = convertBalanceToEther(currentBalance);
     const previousBalanceEther = convertBalanceToEther(previousBalance);
-    const balanceChangeEther = convertBalanceToEther(balanceChange - gasFees);
+    const balanceChangeEther = convertBalanceToEther(balanceChange);
     console.log(
       `Wallet: ${walletAddress}, Timestamp: ${tx.timeStamp.toISOString()}, Current Balance (Ether): ${currentBalanceEther}, Previous Balance (Ether): ${previousBalanceEther}, Balance Change (Ether): ${balanceChangeEther}`,
     );
+
+    // Save balance history to the database
+    await saveBalanceHistoryToDB(walletId, {
+      timeStamp: tx.timeStamp,
+      currentBalance,
+      previousBalance,
+      balanceChange,
+      walletAddress,
+    });
   }
 
   return balanceChanges;
@@ -98,15 +138,78 @@ async function getWalletBalanceHistory(walletAddress: string): Promise<BalanceCh
 
 async function calculateBalancesForAllWallets(): Promise<void> {
   // Get all wallet addresses from the database
-  const walletAddresses = await prisma.wallet.findMany();
+  const walletAddresses = await prisma.wallet.findMany({
+    select: {
+      id: true,
+      address: true,
+      status: true,
+    },
+  });
 
   for (const wallet of walletAddresses) {
-    const walletAddress = wallet.address;
-    console.log(`Fetching balance changes for wallet: ${walletAddress}`);
-    const balanceChanges = await getWalletBalanceHistory(walletAddress);
-    console.log(`Balance changes for wallet: ${walletAddress}`, balanceChanges);
+    try {
+      if (wallet.status === WalletStatus.NEEDS_INDEXING) {
+        continue;
+      } else {
+        if (wallet.status === WalletStatus.INDEXING) {
+          // Wallet is currently being indexed, delete existing data and set to NEEDS_INDEXING
+          await deleteDataForIndexingWallet(wallet);
+        }
+        // Mark the wallet as INDEXING once all processing starts
+        await prisma.wallet.update({
+          where: {
+            address: wallet.address,
+          },
+          data: {
+            status: WalletStatus.INDEXING,
+          },
+        });
+        const walletId = wallet.id;
+        const walletAddress = wallet.address;
+        console.log(`Fetching balance changes for wallet: ${walletAddress}`);
+        const balanceChanges = await getWalletBalanceHistory(walletId, walletAddress);
+        console.log(`Balance history for wallet: ${walletAddress}`);
+
+        for (const change of balanceChanges) {
+          console.log(
+            `Timestamp: ${change.timeStamp.toISOString()}, ` +
+              `Current Balance: ${convertBalanceToEther(change.currentBalance)}, ` +
+              `Previous Balance: ${convertBalanceToEther(change.previousBalance)}, ` +
+              `Balance Change: ${convertBalanceToEther(change.balanceChange)}, `,
+          );
+        }
+        // Mark the wallet as INDEXED once all processing is complete
+        await prisma.wallet.update({
+          where: {
+            address: wallet.address,
+          },
+          data: {
+            status: WalletStatus.NEEDS_INDEXING,
+          },
+        });
+      }
+    } catch (e) {
+      await prisma.wallet.update({
+        where: {
+          address: wallet.address,
+        },
+        data: {
+          status: WalletStatus.ERROR,
+        },
+      });
+
+      console.log(e);
+    }
   }
 }
 
 // Example usage:
-calculateBalancesForAllWallets().catch((e) => console.error(e));
+calculateBalancesForAllWallets()
+  .then(() => {
+    console.log('Balance calculation completed');
+    prisma.$disconnect();
+  })
+  .catch((e) => {
+    console.error(e);
+    prisma.$disconnect();
+  });
